ba9586fac62eca56ae9a250a842b4b159a41ca9d
Mario Brito <mariobrito94@hotmail.com>
2022-03-29 11:26:31 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css'
 import Header from './components/Header';
 import Home from './components/Home';
 
-
-
+function App() {
+    
+    return (
+      <div className="app">
+        <Header />
+      </div>
+    );
+  }
+  
 
 export default App;
